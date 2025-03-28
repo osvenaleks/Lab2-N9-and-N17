482f44e0e400a441d67987b508d760011eb18fc2
osven <osinaleks4@tutamail.com>
2025-03-28 21:31:30 +0300

    update(N9.cpp,N9.h) обЪявил и инициализировал функию для вычисления кол-во элементов меньших P для статических массивов

---
+++ N9.cpp
@@ -0,0 +1,13 @@
+#include <N9.h>
+int32_t CountOfElementsLowerThanP(double arr[],int32_t arrSize,double P,int32_t* elementsLower P){
+	try {
+		for (int32_t i = {};i < arrSize) {
+			if (arr[i] < P) {
+				++elemetnsLowerP;
+			}
+		
+		}
+	}
+	return elementsLowerP;
+}
+
---
+++ N9.h
@@ -0,0 +1,8 @@
+#ifndef N9_H
+#define N9_H
+
+#include <iostream>
+int32_t CountOfElementsLowerThanP(double arr[],int32_t arrSize;
+
+
+#endif
